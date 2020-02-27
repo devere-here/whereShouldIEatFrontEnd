85ba792aba88aea84ad5ffd7d33037c8c9b759a1
Steven DeVere <sdevere@xogrp.com>
2020-02-27 00:02:17 -0500

    Creates Signup and Login components

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	import LoginSignup from './pages/LoginSignup.svelte';
+
+	export let count = 0;
+
+	const increment = () => count += 1
+	const decrement = () => count -= 1
+</script>
+
+<main>
+	<div>
+		<span>Stuffff</span>
+		<LoginSignup />
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/pages/LoginSignup.svelte
@@ -0,0 +1,61 @@
+<script>
+import axios from 'axios'
+  let version = 'login'
+  let username = ''
+  let password = ''
+
+  const handleClick = () => {
+    if (version === 'login') {
+      axios.put('/signup', (req, res) => {
+        console.log('in signup req, res', req, res)
+      })
+    } else {
+      axios.put('/signup', (req, res) => {
+        console.log('in signup req, res', req, res)
+      })
+    }
+  }
+
+  const changeVersion = () => {
+    if (version === 'login') version = 'signup'
+    else version = 'login'
+  }
+</script>
+
+<main>
+	<div>
+    <h3>{version}</h3>
+    <input
+      name="username"
+      placeholder="username"
+    />
+    <input
+      name="password"
+      placeholder="password"
+    />
+    <button on:click={handleClick}>Submit</button>
+    <p on:click={() => changeVersion()}>Don't have an account. Sign in here</p>
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
